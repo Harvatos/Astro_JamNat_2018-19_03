a6bce39242982d6194c25e22201cd7e2e88f37a9
AlexisHarvey <harvey.alexis@hotmail.com>
2019-01-26 11:34:33 -0500

    HD render pipeline + intractable system WIP

---
+++ Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObject : MonoBehaviour
+{
+	public string displayName = "Object";
+	public Rigidbody rb { get; private set; }	
+
+    private void Start()
+    {
+		rb = GetComponent<Rigidbody>();
+		gameObject.layer = 9;
+    }
+}
