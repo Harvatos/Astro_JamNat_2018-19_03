a9de1b8f10eae4ce0da585a3ec99bb191fcd6d2d
Manuel <Manuel.bergeron.d@gmail.com>
2019-01-26 23:28:20 -0500

    lighting, materials and decor

---
+++ Assets/FireStrength.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireStrength : MonoBehaviour
+{
+
+    public float fireStrength;
+
+    public Light fireLight;
+    public Transform flames;
+
+    // Update is called once per frame
+    void Update()
+    {
+        changeScale();
+        changeLight();
+    }
+
+    void changeScale()
+    {
+        flames.localScale = new Vector3(fireStrength, fireStrength, fireStrength);
+    }
+
+    void changeLight()
+    {
+        fireLight.intensity = 250 * fireStrength;
+    }
+}
